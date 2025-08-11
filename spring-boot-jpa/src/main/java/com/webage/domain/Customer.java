d2d673c414c2e14baa0acbdc437926d36d5e7312
Rhea Sridhar <rhea.sridhar@adp.com>
2025-08-11 14:37:34 -0400

    lab 5

---
+++ spring-boot-jpa/src/main/java/com/webage/domain/Customer.java
@@ -0,0 +1,54 @@
+package com.webage.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//	PART 3:
+//	Use an annotation to mark this class as a JPA entity.
+//	Use a separate annotation to override the table name to be "CUSTOMERS".
+//	Use an annotation to mark the id field as the primary key.
+//  Also mark it with @GeneratedValue(strategy=GenerationType.IDENTITY).
+//	Use an annotation to override the column name to be "CUSTOMER_NAME".
+
+@Entity
+@Table(name="CUSTOMERS")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	long id;
+
+	@Column(name="CUSTOMER_NAME")
+	String name;
+	
+	String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ spring-boot-jpa/src/main/java/com/webage/repository/CustomersRepository.java
@@ -0,0 +1,12 @@
+package com.webage.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.webage.domain.Customer;
+
+//  PART 5a
+//  Alter this interface to extend CrudRepository<Customer, Long> 
+
+public interface CustomersRepository extends CrudRepository<Customer, Long>
+{
+}
